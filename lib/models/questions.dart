9c62a66492fc0d1432d89300a3919ce2c7f4c8c8
sivakorn28-7 <645051200287@mail.rmutk.ac.th>
2024-02-09 18:31:49 +0700

    first commit

---
+++ lib/models/questions.dart
@@ -0,0 +1,106 @@
+import 'package:ze06_02/models/question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: 'ใครคือผู้ที่ได้รับสมญานามว่า “เทพเจ้าสายฟ้า”?',
+    correctAnswerIndex: 1,
+    options: [
+      'Iron Man',
+      'Thor',
+      'Hulk',
+      'Captain America',
+    ],
+  ),
+  Question(
+    question: 'ตัวละครตัวไหนมีนามแฝงว่า "Wolverine"?',
+    correctAnswerIndex: 0,
+    options: [
+      'Logan',
+      'Bruce Banner',
+      'Peter Parker',
+      'Steve Rogers',
+    ],
+  ),
+  Question(
+    question: 'ข้อใดคือชื่อจริงของ Iron Man ?',
+    correctAnswerIndex: 2,
+    options: [
+      'Clark Kent',
+      'Bruce Wayne',
+      'Tony Stark',
+      'Peter Parker',
+    ],
+  ),
+  Question(
+    question: 'ใครคือศัตรูตัวฉกาจของ Spider-Man?',
+    correctAnswerIndex: 3,
+    options: [
+      'Thanos',
+      'Doctor Octopus',
+      'Venom',
+      'Green Goblin',
+    ],
+  ),
+  Question(
+    question: 'ตัวละครตัวไหนมีชื่อเรียกอีกอย่างว่า “เมอร์คมีปาก”?',
+    correctAnswerIndex: 1,
+    options: [
+      'Black Widow',
+      'Deadpool',
+      'Punisher',
+      'Cable',
+    ],
+  ),
+  Question(
+    question: 'Symbiote เอเลี่ยนผู้มีความรู้สึกผูกพันกับ Eddie Brock ชื่ออะไร?',
+    correctAnswerIndex: 3,
+    options: [
+      'Riot',
+      'Toxin',
+      'Carnage',
+      'Venom',
+    ],
+  ),
+  Question(
+    question: 'ใครคือ Avenger ที่แข็งแกร่งที่สุด?',
+    correctAnswerIndex: 2,
+    options: [
+      'Captain Marvel',
+      'Thor',
+      'Hulk',
+      'Scarlet Witch',
+    ],
+  ),
+  Question(
+    question: 'ตัวละครใดเป็นผู้ปกครองประเทศ Wakanda ในแอฟริกา?',
+    correctAnswerIndex: 1,
+    options: [
+      'Storm',
+      'Black Panther',
+      'War Machine',
+      'Falcon',
+    ],
+  ),
+  Question(
+    question:
+        'เทพเจ้านอร์สแห่งความชั่วร้ายและเป็นน้องชายของ Thor ชื่ออะไร?',
+    correctAnswerIndex: 0,
+    options: [
+      'Loki',
+      'Odin',
+      'Heimdall',
+      'Balder',
+    ],
+  ),
+  Question(
+    question:
+        'ซูเปอร์ฮีโร่คนไหนที่ได้ฉายาว่า "First Avenger"?',
+    correctAnswerIndex: 2,
+    options: [
+      'Black Widow',
+      'Iron Man',
+      'Captain America',
+      'Hawkeye',
+    ],
+  ),
+];
